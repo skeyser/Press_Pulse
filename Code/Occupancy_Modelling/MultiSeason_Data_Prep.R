a4dd73a9dbe3dd55aac8ed55a9d92c6bfa4ebc2f
Spencer Ryan Keyser <srk252@cornell.edu>
2025-10-15 11:31:55 -0500

    Updates for single species dynamic occ mod. Used for initial testing. Old.

--- Code/Occupancy_Modelling/MultiSeason_Data_Prep.R
+++ Code/Occupancy_Modelling/MultiSeason_Data_Prep.R
@@ -184,11 +184,11 @@ y <- array(unlist(sp.det.list.r), dim = c(nsite, nsurv, nyear),
                            c(2021:2024)))
 
 ## Let's get a sense for the number of surveys that have been repeated
-table(nyear <- apply(y, c(1,2), function(x) sum(!is.na(x))))
-nvisits <- apply(y, c(1,3), function(x) sum(!is.na(x)))
-nvisits[nvisits > 0] <- 1
-table(rowSums(nvisits))
-table(nsites <- apply(y, c(2,3), function(x) sum(!is.na(x))))
+# table(nyear <- apply(y, c(1,2), function(x) sum(!is.na(x))))
+# nvisits <- apply(y, c(1,3), function(x) sum(!is.na(x)))
+# nvisits[nvisits > 0] <- 1
+# table(rowSums(nvisits))
+# table(nsites <- apply(y, c(2,3), function(x) sum(!is.na(x))))
 
 
 ## Get the number of hours per survey for the detection covariate
@@ -278,30 +278,23 @@ eff.jday.std <- array(unlist(eff.jday.std), dim = c(nsite, nsurv, nyear),
                                       colnames(eff.jday.std[[1]]),
                                       c(2021:2024)))
 
+## Mask the 0s to NAs since these values correspond to missing biological data
+eff.hrs.std.na <- eff.hrs.std
+eff.hrs.std.na[eff.hrs.std.na == 0] <- NA 
+mean.hrs <- mean(eff.hrs.std.na, na.rm = T)
+sd.hrs <- sd(eff.hrs.std.na, na.rm = T)
 
-eff.dat <- eff.dat[,-1]
-colnames(eff.dat) <- gsub("[[:punct:]]", "_", gsub("X", "", colnames(eff.dat)))
+## Scale by mean and sd
+eff.hrs.std.sc <- (eff.hrs.std - mean.hrs) / sd.hrs
 
-## Summarize the effort data at the same temporal interval
-eff.days <- second_samp(DAT = eff.dat, interval = 6, id_col = "Cell_Unit", eff = T, e.var = "Days")
-colnames(eff.days)[colnames(eff.days) == "V1"] <- "Cell_Unit"
-eff.days <- eff.days[,-1]
-colnames(eff.days) <- NULL
+eff.jday.std.na <- eff.jday.std
+eff.jday.std.na[eff.jday.std.na == 0] <- NA 
+mean.jday <- mean(eff.jday.std.na, na.rm = T)
+sd.jday <- sd(eff.jday.std.na, na.rm = T)
 
-## Summarize the total number of hours surveyed per sampling unit
-eff.hrs <- second_samp(DAT = eff.dat, interval = 6, id_col = "Cell_Unit", eff = T, e.var = "Hrs")
-colnames(eff.hrs)[colnames(eff.hrs) == "V1"] <- "Cell_Unit"
-#eff.hrs[eff.hrs == 0] <- NA
-eff.hrs <- eff.hrs[,-1]
-colnames(eff.hrs) <- NULL
+## Scale by mean and sd
+eff.jday.std.sc <- (eff.jday.std - mean.jday) / sd.jday
 
-
-## Summarize the median Jdate
-eff.jday <- second_samp(DAT = eff.dat, interval = 6, id_col = "Cell_Unit", eff = T, e.var = "FirstJDay")
-colnames(eff.jday)[colnames(eff.jday) == "V1"] <- "Cell_Unit"
-#eff.jday[is.na(eff.days)] <- NA
-eff.jday <- eff.jday[,-1]
-colnames(eff.jday) <- NULL
 
 ## Set y to NA for days without sampling
 # Iterate through the matrix and set corresponding values in the 3D array to NA
@@ -318,7 +311,8 @@ for (i in 1:dim(eff.days.std)[1]) {
   }
 }
 # Print the modified 3D array
-print(y.new)
+print(y.new[,,1])
+print(y.new[,,2])
 
 ## -------------------------------------------------------------
 ##
@@ -334,18 +328,26 @@ print(y.new)
 ## -------------------------------------------------------------
 
 ## Load the fire data
-fire <- readRDS(here("Data/Fire_ARU_21_24.RDS"))
-fsev <- fire$FireSeverity
-fsev <- fsev |> 
-  mutate(FCat1_5 = case_when(Fire_Sev_mean_1_5 == 0 ~ "Unburned",
-                             Fire_Sev_mean_1_5 > 0 & Fire_Sev_mean_1_5 <= 2.25 ~ "Low/Mod",
-                             Fire_Sev_mean_1_5 > 2.25 ~ "High")) |> 
-  mutate(FCat6_10 = case_when(Fire_Sev_mean_6_10 == 0 ~ "Unburned",
-                              Fire_Sev_mean_6_10 > 0 & Fire_Sev_mean_6_10 <= 2.25 ~ "Low/Mod",
-                              Fire_Sev_mean_6_10 > 2.25 ~ "High")) |>
-  mutate(FCat1_10 = case_when(Fire_Sev_mean_6_10 == 0 & Fire_Sev_mean_1_5 == 0 ~ "Unburned",
-                              Fire_Sev_mean_6_10 > 2.25 | Fire_Sev_mean_1_5 > 2.25 ~ "High",
-                              Fire_Sev_mean_6_10 > 0 | Fire_Sev_mean_1_5 > 0 ~ "Low/Mod"))
+fire <- readRDS(here("Data/Fire_ARU_21_24_AllUnitsByYears.RDS"))
+#fsev <- fire$FireSeverity
+fsev <- fire
+
+## Combining output before reclassifying
+# fsev <- fsev |> 
+#   
+# 
+# fsev <- fsev |> 
+#   mutate(FCat1_5 = case_when(Fire_Sev_mean_1_5 == 0 ~ "Unburned",
+#                              Fire_Sev_mean_1_5 > 0 & Fire_Sev_mean_1_5 <= 2.25 ~ "Low/Mod",
+#                              Fire_Sev_mean_1_5 > 2.25 ~ "High")) |> 
+#   mutate(FCat6_10 = case_when(Fire_Sev_mean_6_10 == 0 ~ "Unburned",
+#                               Fire_Sev_mean_6_10 > 0 & Fire_Sev_mean_6_10 <= 2.25 ~ "Low/Mod",
+#                               Fire_Sev_mean_6_10 > 2.25 ~ "High")) |>
+#   mutate(FCat1_10 = case_when(Fire_Sev_mean_6_10 == 0 & Fire_Sev_mean_1_5 == 0 ~ "Unburned",
+#                               Fire_Sev_mean_6_10 > 2.25 | Fire_Sev_mean_1_5 > 2.25 ~ "High",
+#                               Fire_Sev_mean_6_10 > 0 | Fire_Sev_mean_1_5 > 0 ~ "Low/Mod"))
+
+
 
 ## Load the anomaly data
 clim <- read.csv(here("Data/Tmax_Anomaly_ARU_21_24.csv"))
@@ -353,13 +355,13 @@ clim <- read.csv(here("Data/Tmax_Anomaly_ARU_21_24.csv"))
 ## Merge the climate and fire data
 fclim <- clim |> 
   rename("deployment_name" = "dplymn_") |> 
-  left_join(fsev, by = c("deployment_name", "srvy_yr" = "Fire_Sev_SurvExtYr")) |> 
+  #left_join(fsev, by = c("deployment_name", "srvy_yr" = "Fire_Sev_SurvExtYr")) |> 
   rename("Cell_Unit" = "Cll_Unt") |> 
   arrange(Cell_Unit) |> 
   #mutate(LagYr = srvy_yr - Year) |>
   #filter(LagYr == 1) |> 
   filter(Year %in% c(2020:2023)) |> 
-  select(Cell_Unit, srvy_yr, Year, Anomaly, Lat, Long)
+  select(Cell_Unit, srvy_yr, Year, bclim, Anomaly, Lat, Long)
 
 ## Let's make this into an array to match the data
 fclim.arr <- fclim |> 
@@ -372,7 +374,46 @@ fclim.arr <- fclim |>
   tibble::column_to_rownames("Cell_Unit") |> 
   as.matrix()
 
+## Array for the fire data
+## I should remedy some of the issue by creating a unique set of 
+##ARUs and extracting only from those for each year
+## Duplications are a result of small shifts in ARU placement
+fire.arr <- fsev |> 
+  mutate(Cell_Unit = gsub("G\\d{3}_V\\d{1}_", "", deployment_name)) |> 
+  filter(Cell_Unit %in% uniq_units) |> 
+  arrange(Cell_Unit) |> 
+  tidyr::pivot_wider(names_from = Fire_Sev_SurvExtYr,
+                     values_from = Fire_Sev_mean_1_10,
+                     id_cols = "Cell_Unit",
+                     values_fn = mean) |>
+  tibble::column_to_rownames("Cell_Unit") |>
+  as.matrix()
 
+
+## Take the baseline climate for initial occupancy
+bclim <- fclim |> 
+  filter(Cell_Unit %in% uniq_units) |> 
+  select(Cell_Unit, Year, bclim, Lat, Long) |> 
+  distinct() |> 
+  tidyr::pivot_wider(names_from = Year,
+                     values_from = bclim,
+                     id_cols = "Cell_Unit",
+                     values_fn = mean) |>
+  tibble::column_to_rownames("Cell_Unit") |> 
+  as.matrix()
+
+## Forest data
+cancov <- read.csv(here("Data/Cancov_ARU_21_24.csv"))
+cancov <- cancov |> 
+  rename("deployment_name" = "dplymn_") |> 
+  rename("Cell_Unit" = "Cll_Unt") |>
+  filter(Cell_Unit %in% uniq_units) |> 
+  arrange(Cell_Unit) |> 
+  select(Cell_Unit, cancov) |> 
+  group_by(Cell_Unit) |> 
+  summarise(cancov = mean(cancov)) |> 
+  tibble::column_to_rownames("Cell_Unit") |> 
+  as.matrix()
 
 ## -------------------------------------------------------------
 ##
@@ -389,9 +430,12 @@ fclim.arr <- fclim |>
 
 win.data <- list(
   y = y.new,
-  clim = fclim.arr,
-  eff.hrs = eff.hrs.std,
-  eff.jday = eff.jday.std,
+  bclim = bclim,
+  cc = cancov,
+  anom = fclim.arr,
+  fire = fire.arr,
+  eff.hrs = eff.hrs.std.sc,
+  eff.jday = eff.jday.std.sc,
   nsites = nsite,
   nyears = nyear,
   nreps = nsurv
@@ -400,3 +444,99 @@ win.data <- list(
 str(win.data)
 
 saveRDS(win.data, file = here("Data/Occ_Data/LABU_Multi_Test.RDS"))
+
+## -------------------------------------------------------------
+##
+## Begin Section: Reformat and save data for Unmarked/UBMS model
+##
+## -------------------------------------------------------------
+
+## Reshape detection data to matrix format (sites x years*visits)
+y_ubms <- matrix(NA, nrow = nsite, ncol = nyear * nsurv)
+for(t in 1:nyear) {
+  col_start <- (t-1) * nsurv + 1
+  col_end <- t * nsurv
+  y_ubms[, col_start:col_end] <- y.new[,,t]
+}
+
+## Create site covariates dataframe
+siteCovs <- data.frame(
+  init_temp = bclim[,1],
+  init_fire = fire.arr[,1]# Using first year as example, adjust as needed
+)
+
+## Create yearly site covariates (time-varying)
+yearlySiteCovs <- list(
+  temp_anomaly = fclim.arr[,1:ncol(fclim.arr), drop=FALSE],
+  fire = fire.arr[,1:ncol(fclim.arr), drop=FALSE]
+)
+
+
+## Create observation covariates
+## Need to reshape into matrices with sites x (years*visits)
+effort <- matrix(NA, nrow = nsite, ncol = nyear * nsurv)
+jday <- matrix(NA, nrow = nsite, ncol = nyear * nsurv)
+
+for(t in 1:nyear) {
+  col_start <- (t-1) * nsurv + 1
+  col_end <- t * nsurv
+  effort[, col_start:col_end] <- eff.hrs.std[,,t]
+  jday[, col_start:col_end] <- eff.jday.std[,,t]
+}
+
+obsCovs <- list(
+  effort = effort,
+  jday = jday
+)
+
+## Create unmarkedMultFrame object
+library(unmarked)
+umf <- unmarkedMultFrame(
+  y = y_ubms,
+  siteCovs = siteCovs,
+  yearlySiteCovs = yearlySiteCovs,
+  obsCovs = obsCovs,
+  numPrimary = nyear
+)
+
+# Save the unmarkedMultiFrame object
+saveRDS(umf, file = here("Data/Occ_Data/LABU_Multi_UBMS.RDS"))
+
+## Unmarked model fit
+## Formula
+(fm <- colext(psiformula = ~ init_temp + init_fire,
+              gammaformula = ~ temp_anomaly + fire + temp_anomaly*fire,
+              epsilonformula = ~ temp_anomaly + fire + temp_anomaly*fire,
+              pformula = ~ effort + jday,
+  umf))
+
+smoothed(fm)
+
+nd <- expand.grid(temp_anomaly = seq(-3, 6, length = 100), fire = seq(0, 3, length = 100))
+eps <- predict(fm, type = "ext", newdata = nd, appendData = TRUE)
+gmm <- predict(fm, type = "col", newdata = nd, appendData = TRUE)
+
+ggplot(data = eps, aes(y = fire, x = temp_anomaly, fill = Predicted)) + 
+  geom_raster() + 
+  scale_fill_viridis_c() + 
+  xlab("Max Temp. Anomaly") + 
+  ylab("Fire Severity") + 
+  labs(fill = "Extinction Prob.")
+
+ggplot(data = gmm, aes(y = fire, x = temp_anomaly, fill = Predicted)) + 
+  geom_raster() + 
+  scale_fill_viridis_c() + 
+  xlab("Max Temp. Anomaly") + 
+  ylab("Fire Severity") + 
+  labs(fill = "Colonization Prob.")
+
+
+
+## UBMS model fit
+fit_labu <- ubms::stan_colext(psiformula = ~ init_temp,
+                              gammaformula = ~ temp_anomaly,
+                              epsilonformula = ~ temp_anomaly,
+                              pformula = ~ effort + jday,
+                              data = umf,
+                              chains = 3,
+                              iter = 3000)
